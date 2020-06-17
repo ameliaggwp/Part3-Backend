aec2d5a9bb06aee8b48c66cc870d4dbd21ec6dbe
unknown <ameliaggwp@gmail.com>
2020-06-17 15:55:09 -0700

    Adding build folder

---
+++ build/precache-manifest.fe3a6f9f7bedb34710525022d7d9a221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6332803d4a8ff874e2f0f27b4cc89a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd04bdb977234cf7e6f5",
+    "url": "/static/css/main.4e69ff31.chunk.css"
+  },
+  {
+    "revision": "7cee364908b574bffa22",
+    "url": "/static/js/2.1d38a834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d38a834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd04bdb977234cf7e6f5",
+    "url": "/static/js/main.f4935eea.chunk.js"
+  },
+  {
+    "revision": "700c674e5282f79f804a",
+    "url": "/static/js/runtime-main.ae5c0632.js"
+  }
+]);
